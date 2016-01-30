5bac1254556dbf916c9f42e25cf86a733a8d47f1
Ilyas Shafigin <ilyas174@gmail.com>
2016-01-30 10:40:36 +0500

    Прототип #1

---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetManager : MonoBehaviour {
+
+	public int plantCount = 36;
+	public float radius;
+
+	private PlantManager[] plants;
+
+	private float sectorAngle;
+
+	// Use this for initialization
+	void Start () {
+		this.plants = new PlantManager[this.plantCount];
+
+		this.sectorAngle = 360f / this.plantCount;
+
+		for (int i = 0; i < this.plantCount; i++) {
+			float angle = i * this.sectorAngle;
+			float x = this.radius * Mathf.Cos (angle * Mathf.Deg2Rad);
+			float y = this.radius * Mathf.Sin (angle * Mathf.Deg2Rad);
+
+			PlantManager plant = PlantManager.CreatePlant ();
+			plant.transform.SetParent (this.transform);
+			plant.transform.Translate (new Vector3(x, y, 0));
+			plant.transform.Rotate (new Vector3(0, 0, angle-90));
+			this.plants[i] = plant;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/Scripts/PlantManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlantManager : MonoBehaviour {
+
+	public static Object prefab = Resources.Load ("Plant");
+
+	public enum Type {
+		Tree, Weed, None
+	}
+
+	private Type type;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Initialize() {
+		this.SetType (Type.None);
+	}
+
+	void SetType(Type type) {
+		this.type = type;
+	}
+
+	bool IsNone() {
+		return this.type == Type.None;
+	}
+
+	bool IsTree() {
+		return this.type == Type.Tree;
+	}
+
+	bool IsWeed() {
+		return this.type == Type.Weed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static PlantManager CreatePlant() {
+		GameObject gameObject = (GameObject) Object.Instantiate (prefab);
+		PlantManager plant = gameObject.GetComponent<PlantManager> ();
+		plant.Initialize ();
+		return plant;
+	}
+}
