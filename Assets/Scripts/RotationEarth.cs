661402c1b81f00ef431b9345a8526921086e5777
nazarmax <sloupok5@bk.ru>
2016-01-30 18:03:24 +0600

    обнова

--- Assets/Scripts/RotationEarth.cs
+++ Assets/Scripts/RotationEarth.cs
@@ -6,53 +6,52 @@ public class RotationEarth : MonoBehaviour {
 	private Vector2 currentMousePosition =Vector2.zero;
 	private bool previousFrameMouseDown = false;
 	public Transform Earth;
-    private bool drag = false;
-    private Vector2 previousTouchPosition = Vector2.zero;
-    private Vector2 currentTouchPosition = Vector2.zero;
+//    private bool drag = false;
+//    private Vector2 previousTouchPosition = Vector2.zero;
+//    private Vector2 currentTouchPosition = Vector2.zero;
 
 	void Update () {
 		//Мышь
-//		Vector2 mousePos = new Vector2(Input.mousePosition.x-Screen.width/2, Input.mousePosition.y-Screen.height/2);
-//		if (Input.GetMouseButtonDown(0) && !previousFrameMouseDown){
-//			previousMousePosition = mousePos;
-//			currentMousePosition = mousePos;
-//			previousFrameMouseDown = true;
-//		}
-//		else if (Input.GetMouseButton(0) && previousFrameMouseDown){
-//			previousMousePosition = currentMousePosition;
-//			currentMousePosition = mousePos;
-//		}
-//		else if (!Input.GetMouseButton(0)){
-//			previousFrameMouseDown = false;	
-//		}
-//		Vector2 previousPositionVector = previousMousePosition;	
-//		Vector2 currentPositionVector = currentMousePosition ;
-//		if (previousPositionVector != currentPositionVector && previousFrameMouseDown)		{
-//			float rotationAmount = ReturnSignedAngleBetweenVectors(previousPositionVector,
-//				currentPositionVector);
-//
-//			Earth.transform.RotateAroundLocal(Vector3.forward, rotationAmount * Time.deltaTime);
-//		}
-		//Сенсор
-		if (Input.touchCount == 1) {
-			Touch touch = Input.GetTouch (0);
-			if (touch.phase == TouchPhase.Began) {
-				previousTouchPosition = touch.position;
-				currentTouchPosition = touch.position;
-			} else if (touch.phase == TouchPhase.Moved) {
-				previousTouchPosition = currentTouchPosition;
-				currentTouchPosition = touch;
-				drag = true;
-			} else {
-				drag = false;
-			}
-			Vector2 previousPositionVector2 = previousTouchPosition;	
-			Vector2 currentPositionVector2 = currentTouchPosition ;
-			if (previousPositionVector2 != currentPositionVector2 && drag){
-				float rotationAmount = ReturnSignedAngleBetweenVectors(previousPositionVector2,currentPositionVector2);
-				Earth.transform.RotateAroundLocal(Vector3.forward, rotationAmount * Time.deltaTime);
-			}
+		Vector2 mousePos = new Vector2(Input.mousePosition.x-Screen.width/2, Input.mousePosition.y-Screen.height/2);
+		if (Input.GetMouseButtonDown(0) && !previousFrameMouseDown){
+			previousMousePosition = mousePos;
+			currentMousePosition = mousePos;
+			previousFrameMouseDown = true;
 		}
+		else if (Input.GetMouseButton(0) && previousFrameMouseDown){
+			previousMousePosition = currentMousePosition;
+			currentMousePosition = mousePos;
+		}
+		else if (!Input.GetMouseButton(0)){
+			previousFrameMouseDown = false;	
+		}
+		Vector2 previousPositionVector = previousMousePosition;	
+		Vector2 currentPositionVector = currentMousePosition ;
+		if (previousPositionVector != currentPositionVector && previousFrameMouseDown)		{
+			float rotationAmount = ReturnSignedAngleBetweenVectors(previousPositionVector,
+				currentPositionVector);
+			Earth.transform.RotateAroundLocal(Vector3.forward, rotationAmount * Time.deltaTime);
+		}
+		//Сенсор
+//		if (Input.touchCount == 1) {
+//			Touch touch = Input.GetTouch (0);
+//			if (touch.phase == TouchPhase.Began) {
+//				previousTouchPosition = touch.position;
+//				currentTouchPosition = touch.position;
+//			} else if (touch.phase == TouchPhase.Moved) {
+//				previousTouchPosition = currentTouchPosition;
+//				currentTouchPosition = touch;
+//				drag = true;
+//			} else {
+//				drag = false;
+//			}
+//			Vector2 previousPositionVector2 = previousTouchPosition;	
+//			Vector2 currentPositionVector2 = currentTouchPosition ;
+//			if (previousPositionVector2 != currentPositionVector2 && drag){
+//				float rotationAmount = ReturnSignedAngleBetweenVectors(previousPositionVector2,currentPositionVector2);
+//				Earth.transform.RotateAroundLocal(Vector3.forward, rotationAmount * Time.deltaTime);
+//			}
+//		}
 	}
     private float ReturnSignedAngleBetweenVectors(Vector2 vectorA, Vector2 vectorB)
 	{
