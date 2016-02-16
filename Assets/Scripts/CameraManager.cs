9c22017750619ea0ca94cea0b219100a7b4fe080
Ilyas Shafigin <ilyas174@gmail.com>
2016-02-16 23:08:01 +0500

    Изменен скрипт управления камерой
    Все экраны отображаются на одной сцене. Камера плавно перемещается между
    ними.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour {
+    public GameObject StartMonitor;
+    public GameObject GameMonitor;
+    public GameObject FinalMonitor;
+    public Canvas CanvasMenu;
+
+    private byte Monitor = 0;
+
+    void Start() {
+		Vector3 sm = GameObject.Find ("StartMonitor").transform.position;
+		transform.position = new Vector3 (sm.x,sm.y,-10);
+        StartMonitor.SetActive(true);
+        GameMonitor.SetActive(false);
+        FinalMonitor.SetActive(false);
+        CanvasMenu.enabled = false;
+    }
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Monitor==0){
+            Monitor = 1;
+            StartMonitor.SetActive(false);
+            GameMonitor.SetActive(true);
+        }
+        if (Monitor == 1){
+            CanvasMenu.enabled = true;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0, 0, -10), 0.05f);
+        }
+        if (Monitor == 2){
+            CanvasMenu.enabled = false;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(22, 0, -10), 0.05f);
+        }
+    }
+
+    public void GameOverInCamera() {
+        Monitor = 2;
+        GameMonitor.SetActive(false);
+        FinalMonitor.SetActive(true);
+    }
+}
