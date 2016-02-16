05abb24930dbf9ca2ec7736563d67dcf300ce5e8
Ilyas Shafigin <ilyas174@gmail.com>
2016-02-16 23:19:50 +0500

    Добавлен движущийся фон

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    public GameObject go;
+    public float timeSpawn;
+
+    private float time;
+//    private List<GameObject> ml = new List<GameObject>();
+
+    void Start()
+    {
+        time = timeSpawn;
+    }
+
+    void OnTrigerExit(Collider other)
+    {
+        //Destroy(other.gameObject);
+    }
+
+    void Update()
+    {
+        if (time < timeSpawn)
+            time += Time.deltaTime;
+        else
+        {
+            //if (transform.position.x >= 25)
+            //    transform.Translate(new Vector3(-22, 0, 0));
+            //transform.position = Vector3.Lerp(transform.position, new Vector3(1, 0, 0), 0.001f);
+            time = 0;
+            Instantiate(go, new Vector3(-30, 0, 2.5f), new Quaternion(0, 0, 0, 0));
+        }
+        //transform.position = Vector3.Lerp(transform.position, new Vector3(22, 0, 0), 0.001f);
+    }
+}
